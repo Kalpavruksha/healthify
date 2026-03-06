454490b97a9b553cbb413dc43b20817c5571a5e7
kalpavruksha G <154514800+Kalpavruksha@users.noreply.github.com>
2026-03-06 17:48:22 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath }  from 'url';
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
